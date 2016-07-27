ae282831f74246289627cac7edbdb1fc4b51c09b
Asser Samak <asser@Asser-Samak.local>
2016-07-27 12:55:36 -0700

    Replaced ListView with RecylcerView
    And not using loaders anymore

--- app/src/main/java/com/example/android/q_up/GuestListAdapter.java
+++ app/src/main/java/com/example/android/q_up/GuestListAdapter.java
@@ -2,44 +2,103 @@ package com.example.android.q_up;
 
 import android.content.Context;
 import android.database.Cursor;
+import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.CursorAdapter;
 import android.widget.TextView;
 
 import com.example.android.q_up.data.QueueContract;
 
-/**
- * Created by asser on 7/22/16.
- */
+import java.util.ArrayList;
 
-public class GuestListAdapter extends CursorAdapter{
 
-    public GuestListAdapter(Context context, Cursor c, int flags) {
-        super(context, c, flags);
+public class GuestListAdapter extends RecyclerView.Adapter<GuestListAdapter.GuestViewHolder>{
+
+    private ArrayList<GuestInfo> guestList;
+
+    public GuestListAdapter(Cursor cursor){
+        super();
+        swapCursor(cursor);
     }
 
     @Override
-    public View newView(Context context, Cursor cursor, ViewGroup viewGroup) {
-        View view = LayoutInflater.from(context).inflate(R.layout.guest_list_item, viewGroup, false);
-        return view;
+    public GuestViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        Context context = parent.getContext();
+        LayoutInflater inflater = LayoutInflater.from(context);
+        View view = inflater.inflate(R.layout.guest_list_item, parent, false);
+        GuestViewHolder viewholder = new GuestViewHolder(view);
+        return viewholder;
     }
 
     @Override
-    public void bindView(View view, Context context, Cursor cursor) {
+    public void onBindViewHolder(GuestViewHolder holder, int position) {
 
-        //is there a better pattern to do this lookup ?
-        long id = cursor.getLong(cursor.getColumnIndex(QueueContract.QueueEntry._ID));
-        String name = cursor.getString(cursor.getColumnIndex(QueueContract.QueueEntry.COLUMN_NAME));
-        int party = cursor.getInt(cursor.getColumnIndex(QueueContract.QueueEntry.COLUMN_PARTY));
+        Log.d("Binding","Just called the bind view holder method!");
+        GuestInfo guest = guestList.get(position);
+        if(guest==null)
+            return;
+        long id = guest.id;
+        String name = guest.name;
+        int party = guest.party;
 
-        TextView nameTextView = (TextView) view.findViewById(R.id.name_text_view);
-        TextView partyTextView = (TextView) view.findViewById(R.id.party_text_view);
+        holder.itemView.setTag(id);
+        holder.nameTextView.setText(name);
+        holder.partyTextView.setText("("+party+")");
+    }
 
-        view.setTag(id);
-        nameTextView.setText(name);
-        partyTextView.setText("("+party+")");
+
+    @Override
+    public int getItemCount() {
+        return guestList.size();
+    }
+
+    public void swapCursor(Cursor newCursor) {
+        guestList = new ArrayList<>();
+        while(newCursor.moveToNext()){
+            //is there a better pattern to do this lookup ?
+            long id = newCursor.getLong(newCursor.getColumnIndex(QueueContract.QueueEntry._ID));
+            String name = newCursor.getString(newCursor.getColumnIndex(QueueContract.QueueEntry.COLUMN_NAME));
+            int party = newCursor.getInt(newCursor.getColumnIndex(QueueContract.QueueEntry.COLUMN_PARTY));
+            guestList.add(new GuestInfo(id,name,party));
+        }
+        newCursor.close();
+        this.notifyDataSetChanged();
+
+    }
+
+    class GuestInfo{
+        public GuestInfo(long i,String n,int p){
+            id = i;
+            name = n;
+            party = p;
+        }
+        long id;
+        String name;
+        int party;
+    }
+
+    class GuestViewHolder extends RecyclerView.ViewHolder{
+
+        // Will display the guest name
+        TextView nameTextView;
+        // Will display the party number
+        TextView partyTextView;
+
+        /**
+         * Constructor for our ViewHolder. Within this constructor, we get a reference to our
+         * TextViews and set an onClickListener to listen for clicks. Those will be handled in the
+         * onClick method below.
+         * @param itemView The View that you inflated in
+         *                 {@link GuestListAdapter#onCreateViewHolder(ViewGroup, int)}
+         */
+        public GuestViewHolder(View itemView) {
+            super(itemView);
+
+            nameTextView = (TextView) itemView.findViewById(R.id.name_text_view);
+            partyTextView = (TextView) itemView.findViewById(R.id.party_text_view);
+        }
 
     }
 }
--- app/src/main/java/com/example/android/q_up/MainActivity.java
+++ app/src/main/java/com/example/android/q_up/MainActivity.java
@@ -2,10 +2,13 @@ package com.example.android.q_up;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.StaggeredGridLayoutManager;
+import android.support.v7.widget.helper.ItemTouchHelper;
 import android.util.Log;
 import android.view.View;
-import android.widget.AdapterView;
-import android.widget.ListView;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import com.example.android.q_up.data.QueueDbHelper;
@@ -13,7 +16,7 @@ import com.example.android.q_up.data.QueueDbHelper;
 
 public class MainActivity extends AppCompatActivity {
 
-    private ListView allGuestsListView;
+    private RecyclerView allGuestsListView;
     private TextView newGuestNameView;
     private TextView newPartyCountView;
     private GuestListAdapter cursorAdapter;
@@ -24,29 +27,38 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        allGuestsListView = (ListView) this.findViewById(R.id.all_guests_list);
+        allGuestsListView = (RecyclerView) this.findViewById(R.id.all_guests_list);
         newGuestNameView = (TextView) this.findViewById(R.id.person_name_text);
         newPartyCountView = (TextView) this.findViewById(R.id.party_count_text);
+        allGuestsListView.setLayoutManager(new LinearLayoutManager(this));
 
-        allGuestsListView.setOnItemLongClickListener(new AdapterView.OnItemLongClickListener() {
+
+
+
+        ItemTouchHelper.SimpleCallback simpleItemTouchCallback = new ItemTouchHelper.SimpleCallback(0, ItemTouchHelper.LEFT | ItemTouchHelper.RIGHT) {
+
             @Override
-            public boolean onItemLongClick(AdapterView<?> adapterView, View view, int i, long l) {
-                long myId = (long) view.getTag();
-                boolean success = db.removePerson(myId);
-                cursorAdapter.swapCursor(db.getAllNames());
-                return success;
+            public boolean onMove(RecyclerView recyclerView, RecyclerView.ViewHolder viewHolder, RecyclerView.ViewHolder target) {
+                return false;
             }
-        });
+
+            @Override
+            public void onSwiped(RecyclerView.ViewHolder viewHolder, int swipeDir) {
+                long myId = (long) viewHolder.itemView.getTag();
+                db.removePerson(myId);
+                cursorAdapter.swapCursor(db.getAllNames());
+            }
+        };
+
+        ItemTouchHelper itemTouchHelper = new ItemTouchHelper(simpleItemTouchCallback);
+
+        itemTouchHelper.attachToRecyclerView(allGuestsListView);
 
         db = new QueueDbHelper(this);
-        cursorAdapter = new GuestListAdapter(
-                this,
-                null,
-                0);
+        cursorAdapter = new GuestListAdapter(db.getAllNames());
 
         allGuestsListView.setAdapter(cursorAdapter);
 
-        cursorAdapter.swapCursor(db.getAllNames());
     }
 
     public void addToQ(View view) {
